e6df5fcb0e3cea75fe7a4b9e1ade71cefb10103b
AgroDesu <tehnyaar@yahoo.com>
2020-05-11 11:01:18 -0500

    Adding Tomcat compatibility

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -2,12 +2,13 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Driver {
+public class Driver extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Driver.class, args);
